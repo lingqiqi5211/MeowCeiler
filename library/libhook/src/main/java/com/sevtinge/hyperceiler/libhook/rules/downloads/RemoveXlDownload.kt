7e5ab827d3c683259c4b49e3beca9279ac3cd739
柒柒喵 <lingqiqi233@gmail.com>
2026-04-02 11:49:38 +0800

    try fix: RemoveXlDownload failed to prevent the creation of the .xlDownload folder

--- library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/downloads/RemoveXlDownload.kt
+++ library/libhook/src/main/java/com/sevtinge/hyperceiler/libhook/rules/downloads/RemoveXlDownload.kt
@@ -23,22 +23,44 @@ import io.github.kyuubiran.ezxhelper.core.finder.MethodFinder.`-Static`.methodFi
 import io.github.kyuubiran.ezxhelper.core.util.ClassUtil.loadClass
 import io.github.kyuubiran.ezxhelper.xposed.dsl.HookFactory.`-Static`.createHook
 import io.github.kyuubiran.ezxhelper.xposed.dsl.HookFactory.`-Static`.createHooks
+import java.io.File
 import java.io.IOException
 
 // https://github.com/YifePlayte/WOMMO/blob/6069e7d/app/src/main/java/com/yifeplayte/wommo/hook/hooks/singlepackage/downloadprovider/RemoveXlDownload.kt
 object RemoveXlDownload : BaseHook() {
+    private fun shouldBlock(path: String?): Boolean {
+        return path?.contains("${File.separator}.xlDownload") == true
+    }
+
     override fun init() {
-        loadClass("com.android.providers.downloads.config.XLConfig").methodFinder()
-            .filter { name in setOf("setDebug", "setSoDebug") }
-            .filterNonAbstract().toList()
+        loadClass("java.io.File").methodFinder()
+            .filter { name in setOf("mkdir", "mkdirs") }
+            .toList()
             .createHooks {
-                returnConstant(null)
+                before {
+                    if (shouldBlock((it.thisObject as File).path)) {
+                        it.result = true
+                    }
+                }
+            }
+
+
+        loadClass("com.android.providers.downloads.config.XLConfig")
+            .methodFinder()
+            .filter { name in setOf("setDebug", "setSoDebug") }
+            .toList()
+            .createHooks {
+                before {
+                    if (shouldBlock(it.args[2] as? String)) {
+                        it.result = null
+                    }
+                }
             }
 
         loadClass("com.android.providers.downloads.util.FileUtil").methodFinder()
             .filterByName("createFile").single().createHook {
                 before {
-                    if ((it.args[0] as String).contains(".xlDownload")) {
+                    if (shouldBlock(it.args[0] as String)) {
                         it.throwable = IOException(".xlDownload is blocked")
                     }
                 }
