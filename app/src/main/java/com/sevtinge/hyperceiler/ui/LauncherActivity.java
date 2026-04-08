2c4e01e10b083cbef92ce2a39ae60158cd35e830
柒柒喵 <lingqiqi233@gmail.com>
2026-04-08 13:58:35 +0800

    移除一些多余的内容

--- app/src/main/java/com/sevtinge/hyperceiler/ui/LauncherActivity.java
+++ app/src/main/java/com/sevtinge/hyperceiler/ui/LauncherActivity.java
@@ -2,12 +2,14 @@ package com.sevtinge.hyperceiler.ui;
 
 import android.content.Context;
 
+import com.sevtinge.hyperceiler.utils.LanguageHelper;
+
 import fan.appcompat.app.AppCompatActivity;
 
 public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(com.sevtinge.hyperceiler.utils.LanguageHelper.wrapContext(newBase));
+        super.attachBaseContext(LanguageHelper.wrapContext(newBase));
     }
 }
